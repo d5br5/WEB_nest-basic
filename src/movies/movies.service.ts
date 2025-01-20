bd5ed21eb8af4e6276a52f59bb89e912fb5eacbe
Doh Kim <ehgud456456@naver.com>
2025-01-20 18:16:35 +0900

    feat: create DTO

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity.ts';
+import { CreateMovieDto } from './create-movie.dto.js';
 
 @Injectable()
 export class MoviesService {
@@ -9,21 +10,21 @@ export class MoviesService {
     return this.movies;
   }
 
-  getOne(id: string): Movie {
-    const movie = this.movies.find((movie) => movie.id === +id);
+  getOne(id: number): Movie {
+    const movie = this.movies.find((movie) => movie.id === id);
     if (!movie) {
       throw new NotFoundException(`Movie with ID ${id} not found.`);
     }
     return movie;
   }
 
-  deleteOne(id: string) {
+  deleteOne(id: number) {
     this.getOne(id);
-    this.movies = this.movies.filter((movie) => movie.id !== +id);
+    this.movies = this.movies.filter((movie) => movie.id !== id);
     return true;
   }
 
-  create(movieData) {
+  create(movieData: CreateMovieDto) {
     const newMovie = {
       id: (this.movies[this.movies.length - 1]?.id || 0) + 1,
       ...movieData,
@@ -33,7 +34,7 @@ export class MoviesService {
     return newMovie;
   }
 
-  update(id: string, updateData) {
+  update(id: number, updateData) {
     const movie = this.getOne(id);
     this.deleteOne(id);
     this.movies.push({ ...movie, ...updateData });
